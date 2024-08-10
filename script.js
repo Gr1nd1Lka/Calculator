bfa6eb70b83066e44eb92b89ca79796818e4ecbe
Gr1nd1Lka <george14fed@gmail.com>
2024-08-10 21:05:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,60 +10,76 @@ function deleteLastSymbol() {
     expressionField.focus();  // После удаления фокус остаётся на поле ввода
 }
 
-function replaceSquareSymbols(expression) {
-    // Регулярное выражение для поиска всех степеней
-    return expression.replace(/(\d+)([⁰¹²³⁴⁵⁶⁷⁸⁹]+)/g, function(match, base, exponents) {
-        let exp = '';
-        // Заменяем каждый символ на соответствующее число
-        for (let i = 0; i < exponents.length; i++) {
-            exp += '⁰¹²³⁴⁵⁶⁷⁸⁹'.indexOf(exponents.charAt(i));
-        }
-        return `${base}**${exp}`;
-    });
-}
-
-function formatResult(result) {
-    // Регулярное выражение для замены ** на степени в верхнем регистре
-    return result.replace(/(\d+)\*\*(\d+)/g, function(match, base, exponent) {
-        let expSymbols = '';
-        // Заменяем каждую цифру в экспоненте на соответствующий символ
-        for (let i = 0; i < exponent.length; i++) {
-            expSymbols += '⁰¹²³⁴⁵⁶⁷⁸⁹'.charAt(exponent.charAt(i));
-        }
-        return `${base}${expSymbols}`;
-    });
-}
-
 function calculateExpression() {
     try {
         let expression = document.getElementById('expression').value;
-        // Проверяем, если уравнение содержит '='
+
+        // Проверка на наличие уравнения
         if (expression.includes('=')) {
+            // Удаляем пробелы для корректной работы
+            expression = expression.replace(/\s+/g, '');
+
             let sides = expression.split('=');
-            let leftSide = algebra.parse(sides[0]);
-            let rightSide = algebra.parse(sides[1]);
+            let leftSide = sides[0];
+            let rightSide = sides[1];
+
+            // Преобразуем выражение для корректной интерпретации algebra.js
+            leftSide = algebra.parse(leftSide);
+            rightSide = algebra.parse(rightSide);
+
+            // Создаем уравнение и решаем его
             let equation = new algebra.Equation(leftSide, rightSide);
+            let solution = equation.solveFor("x");
 
-            // Находим все переменные в уравнении
-            let variables = equation.lhs.terms.map(term => term.variables)
-                .concat(equation.rhs.terms.map(term => term.variables))
-                .flat()
-                .filter((v, i, a) => a.indexOf(v) === i);  // Убираем дубликаты
-
-            // Решаем уравнение для каждой переменной и выводим результаты
-            let result = variables.map(variable => {
-                let solution = equation.solveFor(variable);
-                return `${variable} = ${solution.toString()}`;
-            }).join(', ');
-
-            document.getElementById('result').value = result;
+            document.getElementById('result').value = `x = ${solution.toString()}`;
         } else {
             // Обычное вычисление выражения
-            expression = replaceSquareSymbols(expression);
             let result = eval(expression);
-            document.getElementById('result').value = formatResult(result.toString());
+            document.getElementById('result').value = result.toString();
         }
     } catch (error) {
-        document.getElementById('result').value = "Error";
+        console.error(error);
+        document.getElementById('result').value = "Incorrect expression";
     }
 }
+
+const translations = {
+    en: {
+        expressionPlaceholder: "Enter expression",
+        resultPlaceholder: "Result",
+        equalButton: "=",
+        deleteButton: "⌦",
+        numbersHeader: "Numbers",
+        powersHeader: "Powers",
+        incorrectExpression: "Incorrect expression",
+        languageLabel: "Language"
+    },
+    ru: {
+        expressionPlaceholder: "Введите выражение",
+        resultPlaceholder: "Результат",
+        equalButton: "=",
+        deleteButton: "⌦",
+        numbersHeader: "Числа",
+        powersHeader: "Степени",
+        incorrectExpression: "Некорректное выражение",
+        languageLabel: "Язык"
+    }
+};
+
+function switchLanguage(language) {
+    const translation = translations[language];
+
+    document.getElementById('expression').placeholder = translation.expressionPlaceholder;
+    document.getElementById('result').placeholder = translation.resultPlaceholder;
+    document.getElementById('calculate-btn').innerText = translation.equalButton;
+    document.getElementById('delete-btn').innerText = translation.deleteButton;
+
+    document.querySelector('.numbers h3').innerText = translation.numbersHeader;
+    document.querySelector('.powers h3').innerText = translation.powersHeader;
+    document.getElementById('language-label').innerText = translation.languageLabel;
+
+    document.getElementById('result').value = "";
+}
+
+// Инициализация языка по умолчанию (например, английский)
+switchLanguage('en');
